1c441461782f5cb679bade2b0b5754c0a1d4dc88
Xander <gouws.xander@gmail.com>
2025-06-12 20:50:08 -0400

    Prints colors to terminal

--- include/image_processing.h
+++ include/image_processing.h
@@ -1,16 +1,17 @@
-#ifndef IMAGE_PROCESSING
-#define IMAGE_PROCESSING
-#include <stdbool.h>
+// #ifndef IMAGE_PROCESSING
+// #define IMAGE_PROCESSING
+// #include <stdbool.h>
 
-typedef struct {
-    size_t width;
-    size_t height;
-    unsigned char* data;
-} grayscale_image_t;
+// typedef struct {
+//     size_t width;
+//     size_t height;
+//     size_t channels;
+//     unsigned char* data;
+// } image_t;
 
-grayscale_image_t load_image_as_grayscale(const char* file_path);
+// image_t load_image_as_grayscale(const char* file_path);
 
-grayscale_image_t make_resized_grayscale(grayscale_image_t* original, size_t max_width, size_t max_height);
+// image_t make_resized_grayscale(image_t* original, size_t max_width, size_t max_height);
 
-void print_image(grayscale_image_t* image, bool dark_mode);
-#endif
+// void print_image(image_t* image, bool dark_mode);
+// #endif
--- src/main.c
+++ src/main.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "../include/image_processing.h"
+// #include "../include/image_processing.h"
 
 #define DEFAULT_MAX_WIDTH 64
 #define DEFAULT_MAX_HEIGHT 48
 
+// Color ANSI codes
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\033[0m"
+
 
 int main(int argc, char* argv[]) {
-    // Parse arguments
-    if (argc == 1) {
-        printf(
-            "Usage: %s [path/to/image] [max width (default %d)] [max height (default %d)] [dark mode (y/n, default=y)]\n",
-            argv[0], DEFAULT_MAX_WIDTH, DEFAULT_MAX_HEIGHT
-        );
-        return 1;
-    }
+    // // Parse arguments
+    // if (argc == 1) {
+    //     printf(
+    //         "Usage: %s [path/to/image] [max width (default %d)] [max height (default %d)] [dark mode (y/n, default=y)]\n",
+    //         argv[0], DEFAULT_MAX_WIDTH, DEFAULT_MAX_HEIGHT
+    //     );
+    //     return 1;
+    // }
 
-    char* file_path = argv[1];
-    size_t max_width = DEFAULT_MAX_WIDTH, max_height = DEFAULT_MAX_HEIGHT;
-    if (argc >= 3) max_width = (size_t) atoi(argv[2]);
-    if (argc >= 4) max_height = (size_t) atoi(argv[3]);
+    // char* file_path = argv[1];
+    // size_t max_width = DEFAULT_MAX_WIDTH, max_height = DEFAULT_MAX_HEIGHT;
+    // if (argc >= 3) max_width = (size_t) atoi(argv[2]);
+    // if (argc >= 4) max_height = (size_t) atoi(argv[3]);
 
-    bool dark_mode = true;
-    if (argc >= 5) dark_mode = (argv[4][0] == 'y');
+    // bool dark_mode = true;
+    // if (argc >= 5) dark_mode = (argv[4][0] == 'y');
 
-    // Load image
-    grayscale_image_t original = load_image_as_grayscale(file_path);
+    // // Load image
+    // grayscale_image_t original = load_image_as_grayscale(file_path);
 
-    grayscale_image_t resized = make_resized_grayscale(&original, max_width, max_height);
+    // grayscale_image_t resized = make_resized_grayscale(&original, max_width, max_height);
     
-    print_image(&resized, dark_mode);
+    // print_image(&resized, dark_mode);
 
-    return 0;
+    // return 0;
+
+    puts(KRED "This is text is red, " KNRM "this text is normal, " KWHT "and this text is white!\n");
 }
