bbe2ebdaf15f98422887671964c44ad9cc4cc408
Xander <gouws.xander@gmail.com>
2025-06-15 13:32:14 -0400

    Implements sobel edge detection and grayscale conversion

--- include/image.h
+++ include/image.h
@@ -12,9 +12,12 @@ image_t load_image(const char* file_path);
 
 image_t make_resized(image_t* original, size_t max_width, size_t max_height, double character_ratio);
 
+image_t make_grayscale(image_t* original);
+
 double* get_pixel(image_t* image, size_t x, size_t y);
 void set_pixel(image_t* image, size_t x, size_t y, const double* new_pixel);
 
 void get_convolution(image_t* image, double* kernel, double* out);
+void get_sobel(image_t* image, double* out_x, double* out_y);
 
 #endif
