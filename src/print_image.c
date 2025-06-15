e1f1794fbeddb3abc40393bda1efe8e2e39d4432
Xander <gouws.xander@gmail.com>
2025-06-15 13:32:38 -0400

    Adds edge detection in printing images

--- src/print_image.c
+++ src/print_image.c
@@ -16,11 +16,13 @@
 #define CYN "\x1B[36m"
 #define WHT "\x1B[37m"
 
+
 typedef struct {
     double hue;
     double saturation;
     double value;
 } hsv_t;
+
 
 double* get_max(double* a, double* b, double* c) {
     if ((*a >= *b) && (*a >= *c)) {
@@ -41,6 +43,7 @@ double* get_min(double* a, double* b, double* c) {
         return c;
     }
 }
+
 
 hsv_t rgb_to_hsv(double red, double green, double blue) {
     hsv_t hsv;
@@ -73,7 +76,8 @@ hsv_t rgb_to_hsv(double red, double green, double blue) {
     return hsv;
 }
 
-const char* get_color_code(const hsv_t* hsv) {
+
+char* get_color_code(const hsv_t* hsv) {
     if (hsv->saturation < 0.25) {
         return WHT;
     }
@@ -93,10 +97,12 @@ const char* get_color_code(const hsv_t* hsv) {
     }
 }
 
+
 double calculate_grayscale_from_hsv(const hsv_t* hsv) {
     // Use value * value for increased contrast
     return hsv->value * hsv->value;
 }
+
 
 char get_ascii_char(double grayscale) {
     size_t index = (size_t) (grayscale * N_VALUES);
@@ -109,28 +115,64 @@ char get_ascii_char(double grayscale) {
     return VALUE_CHARS[index];
 }
 
+
+char get_sobel_angle_char(double sobel_angle) {
+    if ((22.5 <= sobel_angle && sobel_angle <= 67.5) || (-157.5 <= sobel_angle && sobel_angle <= -112.5))
+        return '\\';
+    else if ((67.5 <= sobel_angle && sobel_angle <= 112.5) || (-112.5 <= sobel_angle && sobel_angle <= -67.5))
+        return '|';
+    else if ((112.5 <= sobel_angle && sobel_angle <= 157.5) || (-67.5 <= sobel_angle && sobel_angle <= -22.5))
+        return '/';
+    else
+        return '_';
+}
+
+
 void print_image(image_t* image) {
+    image_t grayscale = make_grayscale(image);
+    double* sobel_x = calloc(grayscale.width * grayscale.height, sizeof(*sobel_x));
+    double* sobel_y = calloc(grayscale.width * grayscale.height, sizeof(*sobel_y));
+    get_sobel(&grayscale, sobel_x, sobel_y);
+
     for (size_t y = 0; y < image->height; y++) {
         for (size_t x = 0; x < image->width; x++) {
             double* pixel = get_pixel(image, x, y);
-            double grayscale;
+
+            size_t index = y * image->width + x;
+            double sx = sobel_x[index];
+            double sy = sobel_y[index];
+
+            double square_sobel_magnitude = sx * sx + sy * sy;
+            double sobel_angle = atan2(sx, sy) * 180. / M_PI;
+
+            char ascii_char;
+            char* color = WHT;
             
+            double grayscale;
             if (image->channels <= 2) {
                 // Grayscale image
                 grayscale = pixel[0];
+                color = WHT;
             } else {
                 // RGB image
                 hsv_t hsv = rgb_to_hsv(pixel[0], pixel[1], pixel[2]);
                 
-                const char* color = get_color_code(&hsv);
-                printf("%s", color);
-                
                 grayscale = calculate_grayscale_from_hsv(&hsv);
+                color = get_color_code(&hsv);
             }
+
+            ascii_char = get_ascii_char(grayscale);
+
+            // If edge
+            if (square_sobel_magnitude >= 1.5 * 1.5)
+                ascii_char = get_sobel_angle_char(sobel_angle);
             
-            char ascii_char = get_ascii_char(grayscale);
+            printf("%s", color);
             putchar(ascii_char);
         }
         printf("\n");
     }
+
+    free(sobel_x);
+    free(sobel_y);
 }
